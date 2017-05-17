3b24d3e67352f8dae5add8550d7acba60c20551a
pecorina <constanza.pecori@gmail.com>
2017-05-17 12:30:12 -0400

    subiendo ejercicio

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+
+function mauseIn(){
+	alert("estas sobre mi :(")
+}
+function mauseOut(){
+ alert("No estas sobre mi prr prr!")
+}
+
+
+document.getElementById("ñaw").addEventListener("click", function(){
+    
+		alert("hola mundo prr prr")
+	});
